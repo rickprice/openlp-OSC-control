ed6bae233e55e50379c9a5ff7d06c68d24a985f9
Frederick Price <rprice@pricemail.ca>
2024-05-24 16:08:23 -0400

    File Blacked

--- OpenLPRestAPI/__init__.py
+++ OpenLPRestAPI/__init__.py
@@ -1,10 +1,12 @@
+from typing import Any
+
+
 class OpenLPControl:
-    def __init__(self, openlpURL:Str,username:Str, password:Str):
+    def __init__(self, openlpURL: str, username: str, password: str):
         self.controller_themeLevel = ControllerThemeLevel()
         self.controller_currentTheme = ControllerCurrentTheme()
 
         self.plugins_search_options = PluginsSearchOptions()
-
 
         # Authentication Bits
         self.authenticated = False
@@ -19,59 +21,80 @@ class OpenLPControl:
         pass
 
     # API to talk to OpenLP: https://gitlab.com/openlp/wiki/-/wikis/Documentation/HTTP-API
-    def controller_live_items(self): JSon
+    def controller_live_items(self) -> Any:
         pass
-    def controller_live_item(self): JSon
+
+    def controller_live_item(self) -> Any:
         pass
+
     def controller_show(self):
         pass
+
     def controller_progress(self):
         pass
-    def controller_themes(self): JSon
-        pass
-    def controller_theme(self,theme_name: Str):
-        pass
-    def controller_live_theme(self): JSon
-        pass
-    def controller_clear(self, controller_name: Str):
+
+    def controller_themes(self) -> Any:
         pass
 
-    def core_display(self,display_mode: Str):
-        pass
-    def core_plugins(self): JSon
-        pass
-    def core_system(self): JSon
-        pass
-    def core_live_image(self): Image
+    def controller_theme(self, theme_name: str):
         pass
 
-    def plugins_add(self,plugin: Str):
-        pass
-    def plugins_live(self,plugin: Str, id: Str):
+    def controller_live_theme(self) -> Any:
         pass
 
-    def service_items(self): JSon
+    def controller_clear(self, controller_name: str):
         pass
-    def service_show(self,id: Str):
+
+    def core_display(self, display_mode: str):
         pass
-    def service_progress(self, action: Str):
+
+    def core_plugins(self) -> Any:
         pass
+
+    def core_system(self) -> Any:
+        pass
+
+    def core_live_image(self) -> Any:
+        pass
+
+    def plugins_add(self, plugin: str):
+        pass
+
+    def plugins_live(self, plugin: str, id: str):
+        pass
+
+    def service_items(self) -> Any:
+        pass
+
+    def service_show(self, id: str):
+        pass
+
+    def service_progress(self, action: str):
+        pass
+
     def service_new(self):
         pass
+
 
 class ControllerThemeLevel:
     def __get__(self):
         pass
-    def __set__(self,themeLevel):
+
+    def __set__(self, themeLevel):
         pass
+
 
 class ControllerCurrentTheme:
     def __get__(self):
         pass
-    def __set__(self,currentTheme):
+
+    def __set__(self, currentTheme):
         pass
+
+
 class PluginsSearchOptions:
     def __get__(self):
         pass
-    def __set__(self,currentTheme):
+
+    def __set__(self, currentTheme: str):
         pass

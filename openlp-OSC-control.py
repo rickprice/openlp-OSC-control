a8a06abc65e4931e46473a34bbe527ab6ef242b1
Frederick Price <rprice@pricemail.ca>
2024-05-27 23:36:32 -0400

    Latest untested changes

--- openlp-OSC-control.py
+++ openlp-OSC-control.py
@@ -1,10 +1,14 @@
 # from typing import Any, Dict
 
 import OpenLPRestAPI as OLP
-import pythonosc
+from pythonosc.dispatcher import Dispatcher
+from pythonosc.osc_server import BlockingOSCUDPServer
 
 
 
+global OpenLP
+
+openLP = None
 
 if __name__ == "__main__":
     authentication = OLP.OpenLPAuthentication("http://localhost:4316/","openlp","TestPassword")
@@ -20,24 +24,25 @@ if __name__ == "__main__":
     # print (f"TestResult: {openLP.controller_show(0)}")
     # print (f"TestResult: {openLP.service_progress('next')}")
     # print (f"TestResult: {openLP.controller_progress('next')}")
-    print (f"TestResult: {openLP.core_display('hide')}")
-    print (f"TestResult: {openLP.core_display('show')}")
+    # print (f"TestResult: {openLP.core_display('hide')}")
+    # print (f"TestResult: {openLP.core_display('show')}")
 
+def OpenLP_core_display(address, *args):
+    openLP.core_display(args[0])
 
-from pythonosc.dispatcher import Dispatcher
-from pythonosc.osc_server import BlockingOSCUDPServer
-
+def OpenLP_controller_progress(_address, *args):
+    openLP.controller_progress(args[0])
 
 def print_handler(address, *args):
     print(f"{address}: {args}")
-
 
 def default_handler(address, *args):
     print(f"DEFAULT {address}: {args}")
 
 
 dispatcher = Dispatcher()
-dispatcher.map("/something/*", print_handler)
+dispatcher.map("/OpenLP/core/display/*", OpenLP_core_display)
+dispatcher.map("/OpenLP/controller/progress/*", OpenLP_controller_progress)
 dispatcher.set_default_handler(default_handler)
 
 ip = "127.0.0.1"

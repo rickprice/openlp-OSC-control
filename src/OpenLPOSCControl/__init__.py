4bb3095e4c5c5508849bb2bb6a129a41c35fadb4
Frederick Price <rprice@pricemail.ca>
2024-05-30 16:06:05 -0400

    This now seems to work with versioning

--- src/OpenLPOSCControl/__init__.py
+++ src/OpenLPOSCControl/__init__.py
@@ -1,3 +1,7 @@
 """
 Module to control OpenLP using OSC commands
 """
+
+# Import the version after writing the version file, to allow for bootstrapping
+from .version import version as __version__
+

87ac77a1c8a9a44afebfca9c9ed1a82b193ea87b
Frederick Price <rprice@pricemail.ca>
2024-06-04 14:07:26 -0400

    Remove version stuff

--- src/OpenLPOSCControl/__init__.py
+++ src/OpenLPOSCControl/__init__.py
@@ -1,7 +1,3 @@
 """
 Module to control OpenLP using OSC commands
 """
-
-# Import the version after writing the version file, to allow for bootstrapping
-from .version import version as __version__
-
